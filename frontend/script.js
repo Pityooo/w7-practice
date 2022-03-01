35e7d73b73ee9ca83123f0b96c6d32dd86b2ba00
Pityooo <pityooone@gmail.com>
2022-03-01 17:36:25 +0100

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-// GET DATA
+// === GET DATA ===
 
  function Month(name, id, nth, days) {
     this.name = name;
@@ -22,14 +22,14 @@
      new Month("December", "dec", 12, 31)
  ]
 
- console.log(months);
-
-// PREPARE DATA
+// === PREPARE DATA ===
 
 
 
-// COMPONENTS = HTML ELEMENTS WE WOULD LIKE TO ADD TO THE DOCUMENT LATER
+// === COMPONENTS = HTML ELEMENTS WE WOULD LIKE TO ADD TO THE DOCUMENT LATER ===
 
+
+// létrehozzuk a "monthSection" funkcióban a section HTML tag-jét
 const monthSection = (id,h1,days) => {
     return `
     <section id="${id}">
@@ -38,36 +38,74 @@ const monthSection = (id,h1,days) => {
     </section>
     `;
 }
-
+// létrehozzuk a "dayCard" funkcióban a cardok HTML tag-jét
 const dayCard = (year, month, day) => {
     return `
     <div class="card">
         <time>${year}</time>
         <time>${month}</time>
         <time>${day}</time>
+        <button class="card-btn">Get DayName!</button>
     </div>
     `;
 }
-
+// létrehozzuk a "dayCards" funkcióban egy for ciklussal az iterálást a months változóban lévő hónapok napjainak számában, majd minden iteráláskor lefuttatja a "callDayCard" függvényt (callBack)
 const dayCards = (months, callDayCard) => {
-
+    // üres változó (ezt fogjuk feltölteni)
     let toReturn = "";
-
+    // iterálunk a hónapok napjain, majd a "toReturn"-t feltöltjük a "callDayCard" funkcióval (callBack)
     for (let i = 1; i <= months.days; i++) {
         toReturn += callDayCard(2022, months.nth, i);
-        
     }
     return toReturn;
 }
-console.log(dayCards(months[0], dayCard));
 
-// RENDER = ADD THE COMPONENTS TO THE DOCUMENT
+// === RENDER = ADD THE COMPONENTS TO THE DOCUMENT ===
 
-const loadEvent = () => {
+
+// az alávonás "_" azt jelöli, hogy van ott egy paraméter, amit nem szeretnénk használni. (arrow functionnál)
+const loadEvent = _ => {
     
+// === létrehozzuk a HTML kódot, a sectionokkel, és bennük a cardokkal: ===
+
+    let content = "";
+    // iterálunk a "months" objektumok közt
+    for (const month of months) {
+        // az üres contentünket feltöltjük a "monthSection" funkcióval (callBack funkció), aminek harmadik "days" paraméterének a "dayCards" funkciót (callBack) adjuk meg így feltölti a sectionoket, a "dayCard"-ban található elementekkel.
+        content += monthSection(month.id, month.name, dayCards(month, dayCard))
+    }
+    // a "root" id-jü elementbe, kiíratjuk a "content" változó tartalmával (sectionok + cardok)
+    document.getElementById("root").insertAdjacentHTML("beforeend", content)
+
+
+
+// === létrehozunk egy kattintós funkciót (clickEvent, vagy eventHandling): ===
+
+/* // Első variáció (túl sok addEventListener)
+    function cardButtonClickEvent(event) {
+        // az adott "event" szülő (parent) elementjének (jelen esetben a "button") ad egy "clicked" class-t. Ha megint rákattintunk, akkor leveszi (toggle)
+        event.target.parentElement.classList.toggle("clicked")
+    }
+
+    // a "cardList" változóban kiválasztjuk az összes "card" class-ú div-et.
+    const cardList = document.querySelectorAll(".card");
+    // iterálunk a "cardList" azaz az összes card között
+    for (const card of cardList) {
+        // minden "card"-ban lévő "button" elementnek figyeljük a "click" event-jét, és ha történik "click" esemény, akkor lefuttatjuk a "cardButtonClickEvent" funkciót
+        card.querySelector("button").addEventListener("click", cardButtonClickEvent)
+    }
+*/
+
+// Második variáció (1db eventListener-el)
+    function clickEvent(event) {
+        // ha a kiválasztott esemény célja tartalmazza a "card-btn" class-t, akkor teljesül amit az "if"-en belülre írunk
+        if (event.target.classList.contains("card-btn")) {
+            // az esemény céljának (az ami tartalmazza a card-btn class-t) megváltoztatjuk az "innerHTML"ét "Get DayName!"-ról "This button was clicked"-re.
+            event.target.innerHTML = "This button was clicked";
+            //az "event" az egy objektum, amin belül a "target" is egy objektum
+        }
+    }
+    document.addEventListener("click", clickEvent)
 }
 
 window.addEventListener("load", loadEvent);
-
-
-root-ba sectionok, és abba a day-ek
